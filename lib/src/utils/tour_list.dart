8da6aefa65fe0b1ad8f615cc8d8e69d68f2ecc81
Jin young <xmfkdnak98@gmail.com>
2024-06-06 16:21:31 +0900

    feat: 투어 선택용 데이터 추가

---
+++ lib/src/utils/tour_list.dart
@@ -0,0 +1,24 @@
+final List<dynamic> tourList = [
+  {
+    "tourName": "대구의 옛 극장들",
+    "tourCategory" : "건축물",
+    "tourDescription" : "역사속으로 사라진 대구의 극장들",
+    "tourImage":"https://mblogthumb-phinf.pstatic.net/20110807_1/jjhsl11_1312721495453HRBeD_JPEG/00051_s5we.jpg?type=w420",
+    "latlng" : "35.8742,128.5925",
+  },
+  {
+    "tourName": "대구의 옛 거리들(종로)",
+    "tourCategory": "건축물",
+    "tourDescription" : "옛날 종로거리는 어땠을까?",
+    "tourImage":"https://img3.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202011/26/seouleconomy/20201126173025390iuey.jpg",
+    "latlng" : "35.86903938626625, 128.5916780273563",
+  },
+  {
+    "tourName": "테스트",
+    "tourCategory" : "사회",
+    "tourDescription" : "앱 테스트용 선택지입니다.",
+    "tourImage":"https://tecoble.techcourse.co.kr/static/99fec3ebfcfb4f73ff0c646c58169a75/5d9ce/test.jpg",
+    "latlng" : "35.87560503694795, 128.68163406175216",
+  }
+];
+
