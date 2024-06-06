803cba6441775979f7f43056939c2282515c3f1e
Jin young <xmfkdnak98@gmail.com>
2024-06-06 16:42:42 +0900

    feat: 투어 선택 키 추가 및 기타 설정 파일 추가

--- lib/src/utils/secure_storage.dart
+++ lib/src/utils/secure_storage.dart
@@ -2,5 +2,6 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 const ACCESS_TOKEN_KEY = "ACCESS_TOKEN";
 const REFRESH_TOKEN_KEY = "REFRESH_TOKEN";
+const SELETED_TOUR_KEY = "SELETED_TOUR";
 
 final storage = FlutterSecureStorage();
