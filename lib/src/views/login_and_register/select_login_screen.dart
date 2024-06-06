715e7f6208b8838613d67a971582fb6d81d2476e
Jin young <xmfkdnak98@gmail.com>
2024-06-06 16:41:33 +0900

    design: 각 로그인별 버튼에 로고 추가

--- lib/src/views/login_and_register/select_login_screen.dart
+++ lib/src/views/login_and_register/select_login_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:project/src/utils/colors.dart';
+import 'package:project/src/utils/my_flutter_app_icons.dart';
 import 'package:project/src/views/login_and_register/login_screen.dart';
+import 'package:project/src/views/login_and_register/register_email_screen.dart';
 import 'package:project/src/weiget/custom_text_button.dart';
 
 class SelectLoginScreen extends StatelessWidget {
@@ -22,7 +24,14 @@ class SelectLoginScreen extends StatelessWidget {
                 Expanded(
                   flex: 3,
                   child: Center(
-                    child: Text("대구의 유구한 역사와 문화를"),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset("assets/logo.png"),
+                        Text("대구의 유구한 역사와 문화를", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
+                        Text("쉽고 재미있게 배워보세요", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
+                      ],
+                    ),
                   ),
                 ),
                 Expanded(
@@ -30,12 +39,19 @@ class SelectLoginScreen extends StatelessWidget {
                   child: Column(
                     children: [
                       CustomTextButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => RegisterEmailScreen(),
+                            ),
+                          );
+                        },
                         buttonText: "이메일로 시작하기",
                         backgroundColor: PRIMARY_COLOR,
                         foregroundColor: Colors.white,
                         width: double.infinity,
                         height: 45,
+                        icon: Icon(Icons.email_outlined),
                       ),
                       SizedBox(
                         height: 15,
@@ -47,6 +63,7 @@ class SelectLoginScreen extends StatelessWidget {
                         foregroundColor: Colors.black,
                         width: double.infinity,
                         height: 45,
+                        icon: Icon(MyFlutterApp.vector),
                       ),
                       SizedBox(
                         height: 15,
@@ -59,6 +76,7 @@ class SelectLoginScreen extends StatelessWidget {
                         width: double.infinity,
                         height: 45,
                         side: BorderSide(color: Colors.black, width: 1),
+                        icon: Icon(MyFlutterApp.google, color: PRIMARY_COLOR,),
                       ),
                       SizedBox(
                         height: 15,
